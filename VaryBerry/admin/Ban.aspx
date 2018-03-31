ad375e95e6a715ca7608c416965a84b4ad37bacb
장지훈 <s5v7@outlook.kr>
2018-03-31 09:16:28 +0900

    밴 기능 추가

---
+++ VaryBerry/admin/Ban.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master/Navbar-Admin.master" AutoEventWireup="true" CodeBehind="Ban.aspx.cs" Inherits="VaryBerry.admin.Ban" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Header" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Contents" runat="server">
+		<!-- Ban -->
+	<div style="width: 100%; height: 70%; padding-top: 50px; margin-bottom: 150px; padding-left: 100px; padding-right: 100px; text-align: left;">
+		<h3><strong>Ban</strong></h3>
+		<hr class="hr-sky" />
+		<form runat="server" style="height: 100%;">
+			<div style="float: left; margin-left: 50px; margin-right: 30px;">
+				<h4></h4>
+			</div>
+			<div style="float: left; width: 70%; width: calc(100% - 300px); height: 70%;">
+				<asp:TextBox ID="PostID" runat="server" Width="100%" />
+				<br />
+				<br />
+				<asp:Button runat="server" CssClass="btn btn-primary float-right" OnClick="BanAndDelete" Text="등록" />
+			</div>
+		</form>
+	</div>
+</asp:Content>
---
+++ VaryBerry/admin/Ban.aspx.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Web.UI;
+
+namespace VaryBerry.admin {
+	public partial class Ban : Page {
+		protected void Page_Load(object sender, EventArgs e) {
+
+		}
+
+		// Ban and Delete
+		protected void BanAndDelete(object sender, EventArgs e) {
+			// Connect to DB;
+			MySqlConnection conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["VaryBerry"].ConnectionString);
+			conn.Open();
+
+			// 아이디 불러오기
+			string sql = "SELECT UserID FROM questions Where Id=" + PostID.Text.Trim() + ";";
+			// Connect to Database
+			MySqlCommand cmd = new MySqlCommand(sql, conn);
+			string UserID = (string)cmd.ExecuteScalar();
+
+			// 삭제
+			sql = "DELETE FROM questions Where Id=" + PostID.Text.Trim() + ";";
+			cmd.CommandText = sql;
+			cmd.ExecuteNonQuery();
+
+			// 밴
+			if (!Models.BanManager.IsBan(UserID)) {
+				sql = "INSERT INTO ban_list(UserID) VALUES (?);";
+				cmd.CommandText = sql;
+				cmd.Parameters.Add("UserID", MySqlDbType.VarChar).Value = UserID;
+				cmd.ExecuteNonQuery();
+			}
+
+			PostID.Text = "";
+		}
+	}
+}
